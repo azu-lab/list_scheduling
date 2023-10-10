357bc4ef9cb4261ca33673be9da64756be0cc8d0
rokamu623 <r.okamura.061@ms.saitama-u.ac.jp>
2023-10-11 00:39:25 +0900

    docs: comment of dag sched
    Signed-off-by: rokamu623 <r.okamura.061@ms.saitama-u.ac.jp>

--- src/scheduler.py
+++ src/scheduler.py
@@ -50,19 +50,20 @@ class Scheduler:
     def schedule_dag(self, dag: Dag) -> list[Node]:
         unscheduled_tasks: list[Node] = [n for n in dag.nodes]
         wait_tasks: list[Node] = []
-        scheduling_task_dict: dict[int, Node] = {c: None for c in range(self.core_num)} # core-task map: {0: Task, 1: None, 2: Task}
+        scheduling_task_dict: dict[int, Node] = {c: None for c in range(self.core_num)}
         scheduled_tasks: list[Node] = []
         time = 0
 
-        # [v for v in scheduling_task_dict.values() if v is not None] -> [None, None, None] -> no task scheduling
         while(len(unscheduled_tasks) != 0 or len(wait_tasks) != 0 or len([v for v in scheduling_task_dict.values() if v is not None]) != 0):
-            # if finish, push scheduled list and core-task map mark None
             for core, task in scheduling_task_dict.items():
                 if task is not None and time >= task.finish_time:
                     scheduled_tasks.append(task)
                     scheduling_task_dict[core] = None
 
-            # if release, enque wait queue and remove unscheduled list
+            ### ===== difference ===== ###
+
+            # if idx set of scheduled task include prev of task, enque wait queue and remove unscheduled list
+            # finish: {0, 1, 2}, prev: {0, 1} -> append
             for task in unscheduled_tasks:
                 if set([n.idx for n in scheduled_tasks]) >= task.prev:
                     wait_tasks.append(task)
@@ -70,11 +71,10 @@ class Scheduler:
                 if task in unscheduled_tasks:
                     unscheduled_tasks.remove(task)
 
-            # sort for priority, defalut -> release_time
+            ### ===== difference ===== ###
+
             wait_tasks.sort(key=lambda x: x.release_time)
 
-            # if any core empty, deque wait queue and map core-task map
-            # this time, start time <- time, finish time <- time+wcet, core <- core
             for core in scheduling_task_dict.keys():
                 if scheduling_task_dict[core] is None and len(wait_tasks) > 0:
                     new_scheduling_task = wait_tasks.pop(0)
