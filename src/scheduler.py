2a0f4c38e03dce35acd33d18dcd4fc09c216c368
rokamu623 <r.okamura.061@ms.saitama-u.ac.jp>
2023-10-10 23:25:41 +0900

    docs: comment
    Signed-off-by: rokamu623 <r.okamura.061@ms.saitama-u.ac.jp>

--- src/scheduler.py
+++ src/scheduler.py
@@ -8,23 +8,29 @@ class Scheduler:
     def schedule(self, tasks: list[Task]) -> list[Task]:
         unscheduled_tasks: list[Task] = tasks
         wait_tasks: list[Task] = []
-        scheduling_task_dict: dict[int, Task] = {c: None for c in range(self.core_num)}
+        scheduling_task_dict: dict[int, Task] = {c: None for c in range(self.core_num)} # core-task map: {0: Task, 1: None, 2: Task}
         scheduled_tasks: list[Task] = []
         time = 0
 
+        # [v for v in scheduling_task_dict.values() if v is not None] -> [None, None, None] -> no task scheduling
         while(len(unscheduled_tasks) != 0 or len(wait_tasks) != 0 or len([v for v in scheduling_task_dict.values() if v is not None]) != 0):
+            # if release, enque wait queue and remove unscheduled list
             for task in unscheduled_tasks:
                 if time >= task.release_time:
                     wait_tasks.append(task)
                     unscheduled_tasks.remove(task)
 
+            # sort for priority, defalut -> release_time
             wait_tasks.sort(key=lambda x: x.release_time)
 
+            # if finish, push scheduled list and core-task map mark None
             for core, task in scheduling_task_dict.items():
                 if task is not None and time >= task.finish_time:
                     scheduled_tasks.append(task)
                     scheduling_task_dict[core] = None
 
+            # if any core empty, deque wait queue and map core-task map
+            # this time, start time <- time, finish time <- time+wcet, core <- core
             for core in scheduling_task_dict.keys():
                 if scheduling_task_dict[core] is None and len(wait_tasks) > 0:
                     new_scheduling_task = wait_tasks.pop(0)
@@ -33,6 +39,7 @@ class Scheduler:
                     new_scheduling_task.core = core
                     scheduling_task_dict[core] = new_scheduling_task
 
+            # step forward
             time += 1
 
         return scheduled_tasks
